8a6dd7f37ed7f499c59e6094035184e8669c264c
banu karthik <priyamurugan.mca13@gmail.com>
2026-04-09 21:18:53 +0530

    final

--- settings.py
+++ settings.py
@@ -66,14 +66,14 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
+CSRF_TRUSTED_ORIGINS = ['https://web-production-72b91.up.railway.app']
 if not DEBUG:
     SECURE_HSTS_SECONDS = 31536000
     SECURE_HSTS_INCLUDE_SUBDOMAINS = True
     SECURE_SSL_REDIRECT = False
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
-    CSRF_TRUSTED_ORIGINS = ['https://web-production-72b91.up.railway.app']
+    
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
